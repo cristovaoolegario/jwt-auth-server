a9abac2d7b53cb2885bd3aad3f8f36a5589c7d16
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-10-09 22:37:01 -0300

    Add validation to check is the register email is already being used.

--- service/user_service.go
+++ service/user_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
 	"github.com/cristovaoolegario/free-auth-server/dto"
 	"github.com/cristovaoolegario/free-auth-server/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -17,9 +19,22 @@ func ProvideUserService(db *mongo.Database) UserService {
 
 func (service *UserService) CreateNewUser(user dto.InsertUser) (*models.User, error) {
 	userToInsert := user.ConvertToUser()
+
+	if _, err := service.GetUserByEmail(userToInsert.Email); err == nil{
+		return nil, errors.New("This email is already being used by an account.")
+	}
+
 	_, err := service.InsertOne(context.TODO(), &userToInsert)
 	if err != nil {
 		return nil, err
 	}
 	return &userToInsert, nil
+}
+
+func (service *UserService) GetUserByEmail(email string) (*models.User, error){
+	user := models.User{}
+	if err := service.FindOne(context.TODO(), bson.M{"email": email}).Decode(&user); err != nil{
+	 	return nil, err
+	}
+	return &user, nil
 }
