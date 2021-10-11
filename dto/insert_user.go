ffc9a43af737f5e0c9631f4cbdd0a78ff22257e2
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-10-10 23:59:19 -0300

    Add email validation.

--- dto/insert_user.go
+++ dto/insert_user.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cristovaoolegario/free-auth-server/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
+	"regexp"
 )
 
 type InsertUser struct {
@@ -33,11 +34,19 @@ func (user *InsertUser) Validate() error {
 	if len(user.Password) == 0 {
 		return MissingFieldError("Password")
 	}
+	if !isValidEmail(user.Email){
+		return errors.New("Invalid email.")
+	}
 	return nil
 }
 
 func MissingFieldError(missingField string) error {
 	return errors.New(missingField + " is required.")
+}
+
+func isValidEmail(email string) bool {
+	r, _ := regexp.Compile("/^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.([a-z]+)?$/i")
+	return r.MatchString(email)
 }
 
 func getEncryptedPassword(password string) []byte {
